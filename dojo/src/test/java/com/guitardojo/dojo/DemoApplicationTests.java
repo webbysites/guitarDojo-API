27382e095d5c2aa27eec7b710ffab1b12daf45f7
Travis Vela <jtravisvela@gmail.com>
2019-12-27 17:20:31 -0600

    basic spring boot setup

---
+++ dojo/src/test/java/com/guitardojo/dojo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guitardojo.dojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
