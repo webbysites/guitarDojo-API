5f312bc239c15445883a8c65c294334c6eb7e12c
Travis Vela <jtravisvela@gmail.com>
2020-01-01 21:49:39 -0600

    add main application

--- dojo/src/main/java/com/guitardojo/dojo/WebConfig.java
+++ dojo/src/main/java/com/guitardojo/dojo/WebConfig.java
@@ -6,17 +6,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+        registry.addMapping("/**").allowedOrigins("*")
             .allowedMethods("OPTIONS")
             .allowedMethods("POST")
             .allowedMethods("PUT")
             .allowedMethods("DELETE")
             .allowedMethods("GET")
             .allowedMethods("HEAD")
-            .allowedMethods("PATCH");
+            .allowedMethods("PATCH")
+            .allowedHeaders("*");
     }
     
 }
