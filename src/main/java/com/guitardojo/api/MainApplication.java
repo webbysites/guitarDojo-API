49a398ffba82aa44f6a37f20a4a8dcddb8187b8d
Travis Vela <jtravisvela@gmail.com>
2020-02-04 11:29:15 -0600

    update main app file

--- src/main/java/com/guitardojo/api/MainApplication.java
+++ src/main/java/com/guitardojo/api/MainApplication.java
@@ -2,17 +2,24 @@ package com.guitardojo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = {"com.guitardojo.dojo.repositories"})
+@EnableMongoRepositories(basePackages = {"com.guitardojo.api.repositories"})
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
+    
+    @Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
